35069b49ee418c57db2b27e50ab6760fd4e75ac8
Pancake1432 <keppabig@gmail.com>
2026-02-18 19:09:09 +0200

    Register User Removed
    Removed the register user page to make it more simple & not a must have for our site

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
 import Dashboard from './pages/Dashboard';
 import ApplicationForm from './pages/ApplicationForm';
 
@@ -11,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/apply" element={<ApplicationForm />} />
       </Routes>
